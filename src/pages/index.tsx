7115a558396dc79ebf0330c6f4905367472b39ee
Darius Foodeei <darius.foodei@gmail.com>
2023-02-12 00:55:23 -0500

    init front3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,22 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Supercharge from "../components/Supercharge";
 import FullyIntegrated from "../components/FullyIntegrated";
 import Footer from "../components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    
-      <div className="w-full">
-      <Header/>
-      <Hero/>
-      <Supercharge/>
-      <FullyIntegrated/>
-      <Footer/>
-      </div>
- 
-  )
+    <div className="w-full">
+      <Header />
+      <Hero />
+      <Supercharge />
+      <FullyIntegrated />
+      <Footer />
+    </div>
+  );
 }
