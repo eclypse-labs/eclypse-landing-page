bb51fc9d82b490e287aa52cf78a3392af958f7a2
ltran <loris.tran@epfl.ch>
2023-03-16 22:53:44 +0100

    fix : header was superposing with text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { HeaderTitle, HeaderButton } from "./Component_bank";
 
 const Header: React.FC = () => {
   return (
-    <div className="h-[100px] flex flex-wrap flex-row items-center justify-between px-10 max-w-[1512px] z-50">
+    <div className="h-[100px] flex flex-row w-full items-center justify-between px-10 max-w-[1512px] z-50">
       <Image src={logo} alt="Eclypse Logo" className="z-50" />
       <div className="flex flex-row items-center gap-10">
         <HeaderTitle message="Github" href="https://github.com/eclypse-labs" />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,32 +7,38 @@ import DesktopOnly from "./DesktopOnly";
 
 export default function Hero() {
   return (
-    <div className="md:h-screen flex flex-col items-center justify-around py-10 px-10 max-w-[1512px] -mt-[100px] z-0">
-      <div className="flex flex-row w-full h-full">
-        <div className="w-4/12 flex flex-wrap flex-col h-full gap-8 text-5xl pl-[10vw] justify-center items-start font-bold ">
-          <div>Collect Fees.</div>
-          <div>
-            Stay <span className="text-primary3">Liquid</span>
-          </div>
-          <div>Level up.</div>
-        </div>
-        <DesktopOnly className="flex items-center justify-center w-full">
-            <Hero3dScene />
-        </DesktopOnly>  
-      </div>
-      <div className="flex flex-col gap-4 items-center justify-center">
-        <div className="text-lg">
-          Sign up for early access and exclusive updates from Eclypse. This
-          website is still a work in progress so it may appear differently on
-          your device.
+    <div className="md:h-screen flex flex-col items-center justify-around pb-10 px-10 max-w-[1512px] z-0">
+    <div className="flex flex-row items-center justify-center w-full h-full">
+      <div className="hidden md:flex flex-wrap flex-col h-full w-[35%] gap-8 text-5xl pl-[10vw] justify-center items-start font-bold ">
+        <div>
+          Collect Fees.
         </div>
         <div>
-          <HeroButton
-            message="JOIN WAITLIST"
-            href="https://yq4evf4qrme.typeform.com/to/StUSjVb1"
-          />
+          Stay <span className="text-primary3">Liquid</span>
         </div>
+        <div>
+          Level up.
+          </div>
+      </div>
+      <div className="flex items-center justify-center w-[65%]">
+        <DesktopOnly className="flex items-center w-full h-[400px] lg:h-[600px] justify-center">
+            <Hero3dScene/>
+        </DesktopOnly>  
       </div>
     </div>
+    <div className="flex flex-col gap-4 items-center w-full justify-center">
+      <div className="text-lg w-[80%] md:w-full text-center">
+        Sign up for early access and exclusive updates from Eclypse. This
+        website is still a work in progress so it may appear differently on
+        your device.
+      </div>
+      <div>
+        <HeroButton
+          message="JOIN WAITLIST"
+          href="https://yq4evf4qrme.typeform.com/to/StUSjVb1"
+        />
+      </div>
+    </div>
+  </div>
   );
 }
