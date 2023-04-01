d93682fbdf1a00ddf7f1448373d6079e0f3b83a1
Darius Foodeei <darius.foodei@gmail.com>
2023-04-01 11:39:02 +0200

    vercel

--- src/components/Supercharge.tsx
+++ src/components/Supercharge.tsx
@@ -15,9 +15,9 @@ export default function Supercharge() {
         </div>
         <div className="w-full text-center text-base sm:text-base md:text-base lg:text-xl xl:text-2xl">
           <p>
-            As an upcoming official facilitator of the GHO stablecoin, Eclypse
-            enables liquidity providers of UniswapV3 to use their position as
-            collateral to unlock part of their yield generating liquidity.
+            As an upcoming facilitator for the GHO stablecoin, Eclypse enables
+            liquidity providers from Uniswap V3 to use their positions as
+            collateral to unlock their yield generating liquidity.
           </p>
         </div>
       </div>
