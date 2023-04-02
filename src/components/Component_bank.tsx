158d14b8049ab26ae0e2110ea5719b11006829c2
Darius Foodeei <darius.foodei@gmail.com>
2023-04-02 15:53:45 +0200

    fixed text box corner rounding

--- src/components/Component_bank.tsx
+++ src/components/Component_bank.tsx
@@ -3,28 +3,33 @@ import Link from "next/link";
 import Image from "next/image";
 import { StaticImageData } from "next/image";
 
-interface HrefAndMsgProps{
+interface HrefAndMsgProps {
   href: string;
   text: string;
   target: string;
 }
-export function HeaderTitle(props : HrefAndMsgProps) {
+export function HeaderTitle(props: HrefAndMsgProps) {
   return (
-    <Link className="hover:text-primary3 text-gray-100 duration-200 z-50" href={props.href} rel="noopener" target="porps.target">
+    <Link
+      className="hover:text-primary3 text-gray-100 duration-200 z-50"
+      href={props.href}
+      rel="noopener"
+      target="porps.target"
+    >
       {props.text}
     </Link>
   );
 }
 
-interface HeaderButton{
+interface HeaderButton {
   href: string;
   message: string;
   target: string;
 }
-export function HeaderButton(props : HrefAndMsgProps) {
+export function HeaderButton(props: HrefAndMsgProps) {
   return (
     <Link
-      className="text-black flex items-center justify-center hover:bg-primary3 bg-gray-100 duration-300 py-2 px-7 rounded-lg h-[46px] z-50"
+      className="text-black flex items-center justify-center hover:bg-primary3 bg-gray-100 duration-300 py-2 px-7 rounded-2xl h-[46px] z-50"
       href={props.href}
       rel="noopener"
       target={props.target}
@@ -34,20 +39,25 @@ export function HeaderButton(props : HrefAndMsgProps) {
   );
 }
 
-interface hrefAndTitleProps{
+interface hrefAndTitleProps {
   href: string;
   title: string;
   target: string;
 }
 export function FooterTitle(props: hrefAndTitleProps) {
   return (
-    <Link className="hover:text-primary3 duration-200 z-50" href={props.href} rel="noopener" target="props.target">
+    <Link
+      className="hover:text-primary3 duration-200 z-50"
+      href={props.href}
+      rel="noopener"
+      target="props.target"
+    >
       {props.title}
     </Link>
   );
 }
 
-interface HeroButton{
+interface HeroButton {
   href: string;
   title: string;
   target: string;
@@ -56,36 +66,32 @@ interface HeroButton{
 export function HeroButton(props: HrefAndMsgProps) {
   return (
     <div className="w-full h-full">
-      <Link
-        href={props.href}
-        target={props.target}
-        rel="noopener"
-        >
-        <div className="text-black hover:bg-primary7 bg-primary6 duration-300 py-4 px-6 rounded-lg relative">
+      <Link href={props.href} target={props.target} rel="noopener">
+        <div className="text-black hover:bg-primary7 bg-primary6 duration-300 py-4 px-6 rounded-2xl relative">
           <span className="absolute animate-ping -top-2 -left-2 h-5 w-5 rounded-full bg-primary8 opacity-75"></span>
           <span className="absolute -top-1 -left-1 rounded-full h-3 w-3 bg-primary8"></span>
-        {props.text}
+          {props.text}
         </div>
       </Link>
     </div>
   );
 }
 
-interface FullyIntegratedCardProps{
+interface FullyIntegratedCardProps {
   title: string;
   text: string;
 }
 
-export function FullyIntegratedCard(props : FullyIntegratedCardProps) {
+export function FullyIntegratedCard(props: FullyIntegratedCardProps) {
   return (
-    <div className="flex flex-col items-center justify-start rounded-lg bg-primary3 bg-opacity-30 p-5 w-full md:w-[420px] h-full gap-3 text-center">
+    <div className="flex flex-col items-center justify-start rounded-2xl bg-primary3 bg-opacity-30 p-5 w-full md:w-[420px] h-full gap-3 text-center">
       <div className="text-lg md:text-xl font-bold">{props.title}</div>
       <div className="text-m text-gray-200">{props.text}</div>
     </div>
   );
 }
 
-interface FooterSocialProps{
+interface FooterSocialProps {
   href: string;
   logo: StaticImageData;
   alt: string;
@@ -94,11 +100,14 @@ interface FooterSocialProps{
 export function FooterSocial(props: FooterSocialProps) {
   return (
     <div className="text-red-200 fill-current">
-    <Link href={props.href} target="blank" rel="noopener" className="hover:opacity-40 duration-300 ">
-
-      <Image src={props.logo} alt={props.alt} className=""/>
-
-    </Link>
+      <Link
+        href={props.href}
+        target="blank"
+        rel="noopener"
+        className="hover:opacity-40 duration-300 "
+      >
+        <Image src={props.logo} alt={props.alt} className="" />
+      </Link>
     </div>
   );
 }
